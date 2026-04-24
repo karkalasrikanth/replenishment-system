02607fcec637fcea38305309481b453e49303f8f
karkalasrikanth <karkala.srikanth@gmail.com>
2026-04-23 23:54:01 -0400

    public subnet created

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -8,19 +8,33 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+# AZ 1 Public Subnet
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+  }
+}
+
+# AZ 2 Public Subnet
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
 
   tags = {
     Name = "private-subnet"
